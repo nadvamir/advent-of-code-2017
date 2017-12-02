ab703a9e0f1daddab9216fb54a1200bec5a839bc
Maksim Solovjov <himaxx2@gmail.com>
2017-12-02 17:13:22 +0000

    Add testing to the first captcha solution

--- 01/sum.cpp
+++ 01/sum.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
-int main() {
-    std::string input;
-    getline(std::cin, input);
-
+size_t captcha(const std::string input) {
     size_t sum = 0;
     for (size_t i = 0; i < input.size(); ++i) {
         if (input[i] == input[(i + 1) % input.size()]) {
             sum += input[i] - '0';
         }
     }
-
-    std::cout << sum << "\n";
+    return sum;
 }
+
+//------------------------------------------------------------------------------
+#define TEST(a) { std::cout << #a << " == " << (a) << std::endl; assert((a)); }
+
+void test() {
+    TEST(captcha("1122") == 3);
+    TEST(captcha("1111") == 4);
+    TEST(captcha("1234") == 0);
+    TEST(captcha("9129") == 9);
+}
+
+void run() {
+    std::string input;
+    getline(std::cin, input);
+    std::cout << captcha(input) << "\n";
+}
+
+int main() {
+    test();
+    run();
+}
+
