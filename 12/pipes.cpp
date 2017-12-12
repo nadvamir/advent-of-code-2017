38776c78e3fc7971c9d6868400cc53753030c5af
Maksim Solovjov <himaxx2@gmail.com>
2017-12-12 06:27:17 +0000

    Solve day 12

---
+++ 12/pipes.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+
+using Graph = std::unordered_map<int, std::unordered_set<int>>;
+
+void addNode(Graph& g, const std::string& line) {
+    std::unordered_set<int> nodes;
+    std::regex nodeRx {"\\d+"};
+    std::sregex_token_iterator matches(begin(line), end(line), nodeRx);
+
+    int first = -1;
+    for_each(matches, std::sregex_token_iterator(), [&](const auto& node) {
+            if (first < 0) {
+                first = std::stoi(node);
+                g.insert({first, {}});
+            }
+            else {
+                int inode = std::stoi(node);
+                g[first].insert(inode);
+                g[inode].insert(first);
+            }
+    });
+}
+
+// destroys the graph in the progress
+size_t get_connections(Graph& g, int node) {
+    size_t conns = 1;
+    auto cs = g[node];
+    g.erase(node);
+
+    for (auto n: cs) {
+        if (g.count(n)) {
+            conns += get_connections(g, n);
+        }
+    }
+    return conns;
+}
+
+//------------------------------------------------------------------------------
+#define ASSERT_EQ(a, e) { \
+    std::cout << #a << " == " << (a) << " == " << (e) << std::endl; \
+    assert((a == e)); }
+
+void test() {
+    Graph g;
+    addNode(g, "2 <-> 0, 3, 4");
+    ASSERT_EQ(g.size(), 4);
+    ASSERT_EQ(g[2].size(), 3);
+
+    addNode(g, "0 <-> 2");
+    addNode(g, "1 <-> 1");
+    addNode(g, "3 <-> 2, 4");
+    addNode(g, "4 <-> 2, 3, 6");
+    addNode(g, "5 <-> 6");
+    addNode(g, "6 <-> 4, 5");
+    ASSERT_EQ(g.size(), 7);
+    auto ncs = get_connections(g, 0);
+    ASSERT_EQ(ncs, 6);
+}
+
+void run() {
+    std::string line;
+    Graph g;
+    while (getline(std::cin, line)) addNode(g, line);
+    std::cout << get_connections(g, 0) << "\n";
+}
+
+int main() {
+    test();
+    run();
+}
+
