57e7411d465162c06d9361c2659da2288780ac9b
Maksim Solovjov <himaxx2@gmail.com>
2017-12-29 00:03:15 +0000

    Speed up day 21 down to 0.8s

--- fast-21/fractal2.cpp
+++ fast-21/fractal2.cpp
@@ -5,143 +5,131 @@
 #include <algorithm>
 #include <iterator>
 #include <unordered_map>
+#include <cmath>
 #include <cassert>
 
 using Rulebook = std::unordered_map<std::string, std::string>;
 using Drawing = std::vector<std::string>;
 
-Drawing to_drawing(const std::string& condensed);
-std::string to_string(Drawing d);
-std::vector<std::string> get_keys(Drawing d);
+std::vector<std::string> get_keys(std::string);
+
+std::string remove_slashes(std::string inputD) {
+    inputD.erase(remove(begin(inputD), end(inputD), '/'), end(inputD));
+    return inputD;
+}
 
 Rulebook read_rule_book(std::istream& is) {
     Rulebook rulebook;
     std::string rule, sep, result;
     while (is >> rule >> sep >> result) {
         // pre-generate all rules
-        for (const auto& rotatedRule: get_keys(to_drawing(rule))) {
+        result = remove_slashes(result);
+        for (const auto& rotatedRule: get_keys(remove_slashes(rule))) {
             rulebook.insert({rotatedRule, result});
         }
     }
     return rulebook;
 }
 
-Drawing to_drawing(const std::string& condensed) {
-    Drawing d;
-    auto it = begin(condensed);
-    while (it != end(condensed)) {
-        auto next = find(it, end(condensed), '/');
-        std::string line(it, next);
-        d.push_back(line);
-        it = (next != end(condensed)) ? next + 1 : next;
-    }
-    return d;
+int get_size(const std::string& drawing) {
+    return round(sqrt(drawing.size()));
 }
 
-std::string to_string(Drawing d) {
-    std::ostringstream ss;
-    for (auto it = begin(d); it != end(d); ++it) {
-        if (it == begin(d)) ss << *it;
-        else ss << '/' << *it;
-    }
-    return ss.str();
-}
-
-Drawing empty(size_t N) {
-    Drawing d(N);
-    for (auto& line: d) {
-        line = std::string(N, '.');
-    }
-    return d;
-}
-
-std::vector<Drawing> split(const Drawing& d, size_t N) {
-    int nparts = d.size() / N;
-    if (nparts == 1) {
-        return {d};
-    }
-
-    std::vector<Drawing> result;
-    
-    // create nparts empty grids
-    for (int i = 0; i < nparts * nparts; ++i) {
-        result.push_back(empty(N));
-    }
-
-    // populate the grid
-    for (int i = 0; i < d.size(); ++i) {
-        for (int j = 0; j < d[i].size(); ++j) {
-            int n = i / N * nparts + j / N;
-            result[n][i % N][j % N] = d[i][j];
-        }
-    }
-    return result;
-}
-
-std::string get_line(const std::vector<Drawing>& drawings, int lineN, int nparts) {
-    std::ostringstream line;
-    int i = lineN % drawings[0].size();
-    for (int n = lineN / drawings[0].size() * nparts; nparts--; ++n) {
-        line << drawings[n][i];
-    }
-    return line.str();
-}
-
-Drawing combine(const std::vector<Drawing>& drawings, int nparts) {
-    if (nparts == 1) return drawings[0];
-
-    std::ostringstream hashed;
-    for (int i = 0; i < nparts * drawings[0].size(); ++i) {
-        auto line = get_line(drawings, i, nparts);
-        if (i == 0) hashed << line;
-        else hashed << '/' << line;
-    }
-    return to_drawing(hashed.str());
-}
-
-Drawing rotate(const Drawing& d) {
-    Drawing r = d;
-    int N = d.size();
+// rotate 180
+/* std::string mirror(std::string drawing) { */
+/*     reverse(begin(drawing), end(drawing)); */
+/*     return drawing; */
+/* } */
+std::string flip(std::string d) {
+    std::string f = d;
+    int N = get_size(d);
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            r[j][N - i - 1] = d[i][j];
-        }
-    }
-    return r;
-}
-
-Drawing flip(const Drawing& d) {
-    Drawing f = d;
-    int N = d.size();
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            f[N - i - 1][j] = d[i][j];
+            f[(N - i - 1) * N + j] = d[i * N + j];
         }
     }
     return f;
 }
 
-std::vector<std::string> get_keys(Drawing d) {
+
+std::string rotate(std::string d) {
+    std::string r = d;
+    int N = get_size(d);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            r[j * N + N - i - 1] = d[i * N + j];
+        }
+    }
+    return r;
+}
+
+std::vector<std::string> get_keys(std::string d) {
     std::vector<std::string> keys;
 
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
 
     d = rotate(d); // back to start
     d = flip(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     d = rotate(d);
-    keys.push_back(to_string(d));
+    keys.push_back(d);
     return keys;
+}
+
+std::vector<std::string> split(const std::string& d, size_t N) {
+    int dN = get_size(d);
+    int nparts = dN / N;
+    if (nparts == 1) {
+        return {d};
+    }
+
+    std::vector<std::string> result;
+    
+    // create nparts empty grids
+    for (int i = 0; i < nparts * nparts; ++i) {
+        result.push_back(std::string(N * N, '.'));
+    }
+
+    // populate the grid
+    for (int i = 0; i < dN; ++i) {
+        for (int j = 0; j < dN; ++j) {
+            int n = i / N * nparts + j / N;
+            result[n][i % N * N + j % N] = d[i * dN + j];
+        }
+    }
+    return result;
+}
+
+std::string get_line(const std::vector<std::string>& drawings, int lineN, int nparts) {
+    std::ostringstream line;
+    int dN = get_size(drawings[0]);
+    int i = lineN % dN;
+    for (int n = lineN / dN * nparts; nparts--; ++n) {
+        line << drawings[n].substr(i*dN, dN);
+    }
+    return line.str();
+}
+
+std::string combine(const std::vector<std::string>& drawings, int nparts) {
+    if (nparts == 1) return drawings[0];
+
+    std::ostringstream hashed;
+    int dN = get_size(drawings[0]);
+    for (int i = 0; i < nparts * dN; ++i) {
+        auto line = get_line(drawings, i, nparts);
+        hashed << line;
+    }
+    return hashed.str();
 }
 
 int get_split_size(int size, int divBy) {
@@ -153,27 +141,28 @@ int get_split_size(int size, int divBy) {
     return size / result;
 }
 
-Drawing iteration(Rulebook& rulebook, Drawing in, int divBy) {
-    int splitSize = get_split_size(in.size(), divBy);
+std::string iteration(Rulebook& rulebook, std::string in, int divBy) {
+    const int inSize = get_size(in);
+    int splitSize = get_split_size(inSize, divBy);
 
     auto s = split(in, splitSize);
     transform(begin(s), end(s), begin(s), [&](const auto& d) {
-            const auto ruleIt = rulebook.find(to_string(d));
+            const auto ruleIt = rulebook.find(d);
             if (ruleIt != end(rulebook)) {
-                return to_drawing(ruleIt->second);
+                return ruleIt->second;
             }
             else {
                 return iteration(rulebook, d, divBy);
             }
     });
 
-    const auto combined = combine(s, in.size() / splitSize);
-    rulebook.insert({to_string(in), to_string(combined)});
+    const auto combined = combine(s, inSize / splitSize);
+    rulebook.insert({in, combined});
     return combined;
 }
 
-Drawing iterate(Rulebook r, size_t n) {
-    Drawing d = to_drawing(".#./..#/###");
+std::string iterate(Rulebook r, size_t n) {
+    std::string d = ".#...####";
     while (n--) {
         int divBy = d.size() % 2 == 0 ? 2 : 3;
         d = iteration(r, std::move(d), divBy);
@@ -181,12 +170,8 @@ Drawing iterate(Rulebook r, size_t n) {
     return d;
 }
 
-size_t num_pixels(const Drawing& drawing) {
-    size_t n = 0;
-    for (const auto& l: drawing) {
-        n += count(begin(l), end(l), '#');
-    }
-    return n;
+size_t num_pixels(const std::string& drawing) {
+    return count(begin(drawing), end(drawing), '#');
 }
 
 //------------------------------------------------------------------------------
@@ -195,6 +180,25 @@ size_t num_pixels(const Drawing& drawing) {
     assert((a == e)); }
 
 void test() {
+    ASSERT_EQ(remove_slashes("../.#"), "...#");
+    ASSERT_EQ(get_size(".#.....#."), 3);
+    /* ASSERT_EQ(mirror(".##....#."), ".#....##."); */
+    ASSERT_EQ(flip(".##....#."), ".#.....##");
+    ASSERT_EQ(rotate(".#...####"), "#..#.###.");
+
+    auto splitDrawings = split("#####.......####", 2);
+    ASSERT_EQ(splitDrawings.size(), 4);
+    ASSERT_EQ(splitDrawings[0], "###.");
+    ASSERT_EQ(splitDrawings[1], "##..");
+    ASSERT_EQ(splitDrawings[2], "..##");
+    ASSERT_EQ(splitDrawings[3], "..##");
+
+    ASSERT_EQ(combine(split("#..#........#..#", 2), 2), "#..#........#..#");
+    ASSERT_EQ(combine(split("##.##.........#..###..#.....#...###.", 2), 3),
+                            "##.##.........#..###..#.....#...###.");
+    ASSERT_EQ(combine(split("####............###.##.....#...#.##.", 2), 3),
+                            "####............###.##.....#...#.##.");
+
     std::istringstream ruleInput(
             "../.# => ##./#../...\n"
             ".#./..#/### => #..#/..../..../#..#\n"
@@ -204,38 +208,20 @@ void test() {
     auto rules = read_rule_book(ruleInput);
     ASSERT_EQ(rules.size(), 21);
 
-    ASSERT_EQ(get_split_size(3, 3), 3);
-    ASSERT_EQ(get_split_size(4, 2), 2);
-    ASSERT_EQ(get_split_size(6, 2), 2);
-    ASSERT_EQ(get_split_size(9, 3), 3);
-    ASSERT_EQ(get_split_size(12, 2), 6);
-    ASSERT_EQ(get_split_size(18, 2), 6);
-    ASSERT_EQ(get_split_size(27, 3), 9);
-    ASSERT_EQ(get_split_size(36, 2), 18);
-    ASSERT_EQ(get_split_size(54, 2), 18);
-    ASSERT_EQ(get_split_size(81, 3), 27);
-
-    auto d1 = to_drawing(".#/#.");
-    ASSERT_EQ(d1[0][0], '.');
-    ASSERT_EQ(d1[0][1], '#');
-    ASSERT_EQ(d1[1][0], '#');
-    ASSERT_EQ(d1[1][1], '.');
-
-    ASSERT_EQ(to_string(d1), ".#/#.");
-    ASSERT_EQ(to_string(to_drawing(".#./..#/###")), ".#./..#/###");
-
-    auto d3 = to_drawing("#..#/..../..../#..#");
-    auto s = split(d3, 2);
-    ASSERT_EQ(s.size(), 4);
-    ASSERT_EQ(to_string(combine(s, 2)), "#..#/..../..../#..#");
-
-    auto rotated = to_string(rotate(to_drawing(".#./..#/###")));
-    ASSERT_EQ(rotated, "#../#.#/##.");
-    auto flipped = to_string(flip(to_drawing(".#./..#/###")));
-    ASSERT_EQ(flipped, "###/..#/.#.");
+    ASSERT_EQ(get_split_size(12, 2) % 2, 0);
+    ASSERT_EQ(get_split_size(81, 3) % 3, 0);
+    auto largest_divisor = [](int n, int divisor, int step) {
+        for (divisor = divisor + step; divisor < n; divisor += step) {
+            if (n % divisor == 0) return false;
+        }
+        return true;
+    };
+    ASSERT_EQ((largest_divisor(81, get_split_size(81, 3), 3)), true);
+    ASSERT_EQ((largest_divisor(54, get_split_size(54, 2), 2)), true);
+    ASSERT_EQ((largest_divisor(18, get_split_size(18, 2), 2)), true);
 
     auto drawing = iterate(rules, 2);
-    ASSERT_EQ(drawing.size(), 6);
+    ASSERT_EQ(drawing.size(), 6*6);
 
     auto npixels = num_pixels(drawing);
     ASSERT_EQ(npixels, 12);
